f333cb1d075f0378c5e4dff1ca20c000c7f8ee94
Shahirul <shahiruliimalo@gmail.com>
2025-01-13 02:58:33 +0800

    fix:🦄 fix production error

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,13 +1,13 @@
 import Actionbar from "@/components/actionbar";
 import React, { ReactNode } from "react";
 
-const HomeLayout = ({
-  children,
-  showActionBar,
-}: {
+// defining the props type for better clarity and reusability
+type HomeLayoutProps = {
   children: ReactNode;
   showActionBar: boolean;
-}) => {
+};
+
+const HomeLayout = ({ children, showActionBar }: HomeLayoutProps) => {
   return (
     <main className="flex flex-col min-h-screen">
       <section className="flex-1">
