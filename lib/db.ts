30d219b3bdcd1dfb0d6b6792036a977672257794
Shahirul <shahiruliimalo@gmail.com>
2025-02-06 12:20:13 +0800

    refactor: 🦄 improve Prisma client singleton initialization

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
-// prevents multiple instances of Prisma Client in development
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
